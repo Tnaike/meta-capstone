ce6d54efe69f7b71ac56aa43f55d229209b43745
Tnaike <timmynaike@gmail.com>
2023-04-11 09:16:08 +0100

    Nav section styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import '../styles/index.css';
 
 const Nav = () => {
   return (
-    <nav className='nav container'>
-      <div className='logo'>
-        <img src={logo} alt='Little Lemon logo' />
-      </div>
-      <ul>
-        <li>
-          <NavLink to='/'>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to='/about'>About</NavLink>
-        </li>
-        <li>
-          <NavLink to='/menu'>Menu</NavLink>
-        </li>
-        <li>
-          <NavLink to='/booking'>Reservations</NavLink>
-        </li>
-        <li>
-          <NavLink to='/order-online'>Order Online</NavLink>
-        </li>
-        <li className='login'>Login</li>
-      </ul>
-    </nav>
+    <div className='nav-wrap'>
+      <nav className='nav container'>
+        <div className='logo'>
+          <img src={logo} alt='Little Lemon logo' />
+        </div>
+        <ul>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>About</NavLink>
+          </li>
+          <li>
+            <NavLink to='/menu'>Menu</NavLink>
+          </li>
+          <li>
+            <NavLink to='/gallery'>Gallery</NavLink>
+          </li>
+          <li>
+            <NavLink to='/order-online'>Order Online</NavLink>
+          </li>
+          <li>
+            <NavLink to='/booking'>Reservations</NavLink>
+          </li>
+        </ul>
+        <div className='signUp'>Signup</div>
+        <div className='login'>Login</div>
+      </nav>
+    </div>
   );
 };
 
