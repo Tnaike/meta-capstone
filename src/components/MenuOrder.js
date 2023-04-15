bee69e5422859af3d72b735223c59f97cd2861d2
Tnaike <timmynaike@gmail.com>
2023-04-15 18:24:58 +0100

    Added images and create menuOrder layout

--- src/components/MenuOrder.js
+++ src/components/MenuOrder.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import lunchMenu from '../assets/lunch-menu.jpg';
+import dinnerMenu from '../assets/dinner-menu.jpg';
+import mainMenu from '../assets/brunch_menu.jpg';
+import dessertMenu from '../assets/dessert-menu.jpg';
 
 const MenuOrder = () => {
   return (
-    <div>MenuOrder</div>
-  )
-}
+    <div className='order-section mt-2'>
+      <div className='container'>
+        <h1 className='section-title mb-3'>Order for Delivery!</h1>
+        <div className='menu-display-wrap'>
+          <div className='menu-list'>
+            <img src={lunchMenu} alt='lunch menu' />
+            <h4>Lunch Menu</h4>
+          </div>
+          <div className='menu-list'>
+            <img src={dinnerMenu} alt='lunch menu' />
+            <h4>Dinner Menu</h4>
+          </div>
+          <div className='menu-list'>
+            <img src={mainMenu} alt='lunch menu' />
+            <h4>Main Menu</h4>
+          </div>
+          <div className='menu-list'>
+            <img src={dessertMenu} alt='lunch menu' />
+            <h4>Dessert Menu</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MenuOrder
+export default MenuOrder;
