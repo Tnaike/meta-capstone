f63353934e5cfa8cca4fcc8b4d9443ce8fc0ef16
Tnaike <timmynaike@gmail.com>
2023-04-19 04:09:09 +0100

    Added unit test for form component and validation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './components/BookingForm';
 
-test('renders title header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Order for Delivery/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Booking App', () => {
+  it('renders title is present', () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/Order for Delivery/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should render button class and attribute', () => {
+    render(<BookingForm />);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toHaveAttribute('type', 'submit');
+    expect(buttonElement).toHaveClass('button', 'button-primary');
+  });
+
+  it('should render date input field with the required attribute set', () => {
+    render(<BookingForm />);
+
+    const dateInput = screen.getByLabelText('Date');
+    expect(dateInput).toHaveAttribute('type', 'date');
+    expect(dateInput).toHaveAttribute('required');
+  });
+
+  it('should render time input field with the required attribute set', () => {
+    render(<BookingForm />);
+
+    const timeInput = screen.getByLabelText('Time');
+    expect(timeInput).toHaveAttribute('required');
+  });
+
+  it('should render Number of guests input field with the required attribute set', () => {
+    render(<BookingForm />);
+
+    const timeInput = screen.getByLabelText('Number of guests');
+    expect(timeInput).toHaveAttribute('min');
+    expect(timeInput).toHaveAttribute('required');
+  });
 });
